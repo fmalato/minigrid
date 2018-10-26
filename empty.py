fbb6cf9f375c91046c7616cbcc2ff1b331b965e6
freaky1310 <freaky1310@gmail.com>
2018-10-26 19:51:07 +0200

    Added updated minigrid file. Replace it.
    The file has been sligthly modified in order to achieve the desired mobility for the goal square. Replacing it won't cause any problem to the rest of the library.

---
+++ empty.py
@@ -0,0 +1,69 @@
+from gym_minigrid.minigrid import *
+from gym_minigrid.register import register
+
+class EmptyEnv(MiniGridEnv):
+    """
+    Empty grid environment, no obstacles, sparse reward
+    """
+
+    # I've sligthly modified this file. In order to make the program work, 
+    # replace it with the original. It won't affect the rest of the lib.
+
+    def __init__(self, size=8):
+        self.posX = size - 2
+        self.posY = size - 2
+
+        super().__init__(
+            grid_size=size,
+            max_steps=4*size*size,
+            # Set this to True for maximum speed
+            see_through_walls=True
+        )
+
+    def set_posX(self, posX):
+        self.posX = posX
+
+    def set_posY(self, posY):
+        self.posY = posY
+
+    def set_goal(self):
+        self.grid.set(self.posX, self.posY, Goal())
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place the agent in the top-left corner
+        self.start_pos = (1, 1)
+        self.start_dir = 0
+
+        # Place a goal square in the bottom-right corner
+        self.set_goal()
+
+        self.mission = "get to the green goal square"
+
+class EmptyEnv6x6(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=6)
+
+class EmptyEnv16x16(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=16)
+
+register(
+    id='MiniGrid-Empty-6x6-v0',
+    entry_point='gym_minigrid.envs:EmptyEnv6x6'
+)
+
+register(
+    id='MiniGrid-Empty-8x8-v0',
+    entry_point='gym_minigrid.envs:EmptyEnv'
+)
+
+register(
+    id='MiniGrid-Empty-16x16-v0',
+    entry_point='gym_minigrid.envs:EmptyEnv16x16'
+)
